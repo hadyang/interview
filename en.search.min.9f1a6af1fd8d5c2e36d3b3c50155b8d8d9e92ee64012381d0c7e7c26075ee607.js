1aae60b171e49e22cd5c17f09bf6fb7bc00e7084
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-07 06:01:44 +0000

    Deploy hadyang/interview to github.com/hadyang/interview.git:gh-pages

--- en.search.min.9f1a6af1fd8d5c2e36d3b3c50155b8d8d9e92ee64012381d0c7e7c26075ee607.js
+++ en.search.min.9f1a6af1fd8d5c2e36d3b3c50155b8d8d9e92ee64012381d0c7e7c26075ee607.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/interview/flexsearch.min.js');loadScript('/interview/en.search-data.min.383dbc3cc3b4da854ca29fe1a6ca083052af28f7b4d0d29f70502e2dde4cdf25.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/interview/flexsearch.min.js');loadScript('/interview/en.search-data.min.1535b612b187bcc5d3bd9f99c0546db26dc3870f1ef7d4b3315e4655d12e2d41.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
