08405fc5c81d116b69fc51bb55e99e6789246b36
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-12 13:32:35 +0000

    Deploy hadyang/interview to github.com/hadyang/interview.git:gh-pages

--- en.search.min.24b596f3b316e60a159116ba7ba543e3cef6e5e1b21160a80bec345329e89e38.js
+++ en.search.min.24b596f3b316e60a159116ba7ba543e3cef6e5e1b21160a80bec345329e89e38.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/interview/flexsearch.min.js');loadScript('/interview/en.search-data.min.8acdbb928f6da72deddec4de0030ee3d4438896c720f412d99976f1f6e23f1c8.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/interview/flexsearch.min.js');loadScript('/interview/en.search-data.min.64eedcc1c82f1e37cbceaac72550da59f3bc2dac138572682552ef91ff0d96d1.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
